7b47459996ccd62a40d472fb0e467d8c4e08fdfb
Jack Ratcliffe <jacktionman@gmail.com>
2019-06-11 16:39:45 +0100

    Changed a board design

--- src/utils/Board.java
+++ src/utils/Board.java
@@ -118,11 +118,11 @@ public class Board {
 //                      "xxxxxxxxxxx\n" +
 //                      "x         x\n" +
 //                      "x         x\n" +
-//                      "x         x\n" +
 //                      "xxxxx xxxxx\n" +
 //                      "xxxxx xxxxx\n" +
 //                      "xxxxx xxxxx\n" +
-//                      "x         x\n" +
+//                      "xxxxx xxxxx\n" +
+//                      "xxxxx xxxxx\n" +
 //                      "x         x\n" +
 //                      "x         x\n" +
 //                      "xxxxxxxxxxx\n"
