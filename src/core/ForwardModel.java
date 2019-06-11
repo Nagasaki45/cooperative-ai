d629439cdd293119ea8337ecf1466f5cb9a60f3a
Terence Broad <terry.m.broad@gmail.com>
2019-06-11 12:47:02 +0100

    prevent players crossing over each other

--- src/core/ForwardModel.java
+++ src/core/ForwardModel.java
@@ -6,8 +6,23 @@ import utils.Vector2d;
 import java.util.HashMap;
 import java.util.Map;
 
-
 public class ForwardModel {
+
+    private static boolean validCrossing(Map<Integer, Vector2d> playerPositions, Map<Integer, Vector2d> newPlayerPosisitions, Types.ACTIONS[] actions) {
+        if (playerPositions.get(0) == newPlayerPosisitions.get(1)
+                || playerPositions.get(1) == newPlayerPosisitions.get(0)) {
+            if (actions[0] == Types.ACTIONS.ACTION_RIGHT && actions[1] == Types.ACTIONS.ACTION_LEFT
+                    || actions[0] == Types.ACTIONS.ACTION_LEFT && actions[1] == Types.ACTIONS.ACTION_RIGHT
+                    || actions[0] == Types.ACTIONS.ACTION_UP && actions[1] == Types.ACTIONS.ACTION_DOWN
+                    || actions[0] == Types.ACTIONS.ACTION_DOWN && actions[1] == Types.ACTIONS.ACTION_UP) {
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return true;
+        }
+    }
 
     public static void applyActions(GameState gs, Types.ACTIONS[] actions)
     {
@@ -29,9 +44,14 @@ public class ForwardModel {
             newPlayerPosisitions.put(i, targetPos);
         }
 
-        if(newPlayerPosisitions.get(0) != newPlayerPosisitions.get(1)) {
+        System.out.println("playerPositions" + playerPositions.toString());
+        System.out.println("newPlayerPositions" + newPlayerPosisitions.toString());
+
+        if(newPlayerPosisitions.get(0) != newPlayerPosisitions.get(1) && validCrossing(playerPositions, newPlayerPosisitions, actions)) {
             gs.updatePlayerPositions(newPlayerPosisitions);
+
         }
+
 
     }
 }
