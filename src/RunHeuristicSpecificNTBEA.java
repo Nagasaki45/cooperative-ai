78f7a0c4e5d997df5914d73f1470e984aee200b0
Tom Gurion <nagasaki45@gmail.com>
2019-06-12 19:14:31 +0100

    1000 iterations are too much. Reducing to 100

--- src/RunHeuristicSpecificNTBEA.java
+++ src/RunHeuristicSpecificNTBEA.java
@@ -12,7 +12,7 @@ import java.util.Map;
 
 public class RunHeuristicSpecificNTBEA {
 
-    public static final int NUM_OF_EVALUATIONS = 1000;  // Each evaluation averages ticks on all boards
+    public static final int NUM_OF_EVALUATIONS = 100;  // Each evaluation averages ticks on all boards
     public static final double NOISE = 1.0;
     public static final int K_EXPLORE = 2;
     public static final double EPSILON = 0.5;
