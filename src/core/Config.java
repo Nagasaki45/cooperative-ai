b1b145c18c75e2cf08b5ce3fde583944b3ae9d3d
Tom Gurion <nagasaki45@gmail.com>
2019-06-13 11:14:20 +0100

    Generate all data with 200 ticks

--- src/core/Config.java
+++ src/core/Config.java
@@ -5,5 +5,5 @@ public class Config {
     public static final boolean VISUALS = true;
     public static final int FRAME_DELAY = 100;  // Milliseconds
     public static final int MULTI_TESTS_ITERATIONS_PER_BOARD = 100;
-    public static int MAX_GAME_TICKS = 100;
+    public static int MAX_GAME_TICKS = 200;
 }
